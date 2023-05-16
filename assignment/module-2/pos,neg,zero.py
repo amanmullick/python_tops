509d247a8c9b2f56f31a243fd86b4bbfec295257
aman <amanmullick14@gmail.com>
2023-05-16 22:04:07 +0530

    module-2 two files added

---
+++ assignment/module-2/pos,neg,zero.py
@@ -0,0 +1,7 @@
+n = -1
+if n>0:
+    print("positive")
+elif n==0:
+    print("zero")
+else:
+    print("negative")
---
+++ tasks/conditional.py
@@ -0,0 +1,11 @@
+marks = 90
+
+if marks>70:
+    print("distinction")
+elif marks>51 and marks<69:
+    print("1st class")
+elif marks>36 and marks<50:
+    print("2nd class")
+elif marks<=35:
+    print("fail")
+
