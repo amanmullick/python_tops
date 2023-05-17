402e2a978c654f4faa950444c51563c8ccb609d4
aman <amanmullick14@gmail.com>
2023-05-17 20:11:34 +0530

    user side input taken for marks

---
+++ tasks/user_cond.py
@@ -0,0 +1,10 @@
+marks = int(input("enter the marks : "))
+if marks>70:
+    print("distinction")
+elif marks>51 and marks<69:
+    print("1st class")
+elif marks>36 and marks<50:
+    print("2nd class")
+elif marks<=35:
+    print("fail")
+
